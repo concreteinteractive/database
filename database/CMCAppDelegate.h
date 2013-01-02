ab3bb3e8831050951ece1a20bc53bf859499973f
Alexander Ledovskiy <alexander@concreteinteractive.com>
2013-01-02 02:02:49 -0800

    Initial Commit

---
+++ database/CMCAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CMCAppDelegate.h
+//  database
+//
+//  Created by Alexander Ledovskiy on 1/2/13.
+//  Copyright (c) 2013 Alexander Ledovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ database/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  database
+//
+//  Created by Alexander Ledovskiy on 1/2/13.
+//  Copyright (c) 2013 Alexander Ledovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CMCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CMCAppDelegate class]));
+    }
+}
